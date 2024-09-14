2c027b49e7a11fcddb1c4797ff9189491cbf68a4
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2024-09-14 18:37:08 +0600

    Add themes

--- components/LinksDropdown.tsx
+++ components/LinksDropdown.tsx
@@ -1,7 +1,41 @@
 import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { AlignLeft } from "lucide-react";
+import links from "@/utils/links";
+import Link from "next/link";
 
 const LinksDropdown = () => {
-  return <div>linksdropdown</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="lg:hidden">
+        <Button variant="outline" size="icon">
+          <AlignLeft />
+          <span className="sr-only">Toggle links</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-52 lg:hidden "
+        align="start"
+        sideOffset={25}
+      >
+        {links.map((link) => {
+          return (
+            <DropdownMenuItem key={link.href}>
+              <Link href={link.href} className="flex items-center gap-x-2 ">
+                {link.icon} <span className="capitalize">{link.label}</span>
+              </Link>
+            </DropdownMenuItem>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 
 export default LinksDropdown;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,10 +1,34 @@
-"use client"
-
-
+"use client";
+import Logo from "@/assets/logo.svg";
+import links from "@/utils/links";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
 import React from "react";
+import { Button } from "./ui/button";
+import Link from "next/link";
 
 const Sidebar = () => {
-  return <div>sidebar</div>;
+  const pathname = usePathname();
+  return (
+    <aside className="py-4 px-8 bg-muted h-full">
+      <Image src={Logo} alt="logo" className="mx-auto" />
+      <div className="flex flex-col mt-20 gap-y-4">
+        {links.map((link) => {
+          return (
+            <Button
+              asChild
+              key={link.href}
+              variant={pathname === link.href ? "default" : "link"}
+            >
+              <Link href={link.href} className="flex items-center gap-x-2 ">
+                {link.icon} <span className="capitalize">{link.label}</span>
+              </Link>
+            </Button>
+          );
+        })}
+      </div>
+    </aside>
+  );
 };
 
 export default Sidebar;
