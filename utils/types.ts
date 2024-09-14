32bb72432ecf33c03bc12a9dcffa8a2ea2176899
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2024-09-15 00:14:53 +0600

    Adding form

---
+++ utils/types.ts
@@ -0,0 +1,28 @@
+import * as z from "zod";
+export type JobType = {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+  clerkId: string;
+  position: string;
+  company: string;
+  location: string;
+  status: string;
+  mode: string;
+};
+
+export enum JobStatus {
+  Pending = "pending",
+  Interview = "interview",
+  Declined = "declined",
+}
+
+export enum JobMode {
+  FullTime = "full-time",
+  PartTime = "part-time",
+  Internship = "internship",
+}
+
+export const createAndEditJobSchema = z.object({
+  position: z.string().min(3, {}),
+});
